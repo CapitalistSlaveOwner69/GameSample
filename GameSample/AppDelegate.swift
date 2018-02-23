118f35feaab77b78d8853793a525d5df1539dba2
Anna <YuanfangWang@bobwangs-macbook-air.local>
2018-02-23 13:08:05 -0600

    Initial Commit

---
+++ GameSample/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  GameSample
+//
+//  Created by Yuanfang Wang on 2/23/18.
+//  Copyright © 2018 Yuanfang Wang. All rights reserved.
+//
+
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
